b358719da6e1ac0fffa28a43a7c3b748c893beac
Theodor Vararu <theo@vararu.org>
2020-10-02 20:24:50 +0100

    Update pamac.sh

--- pamac.sh
+++ pamac.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+sudo sed -i "s@#EnableAUR@EnableAUR@" /etc/pamac.conf
+sudo sed -i "s@#CheckAURUpdates@CheckAURUpdates@" /etc/pamac.conf
+sudo sed -i "s@#CheckAURVCSUpdates@CheckAURVCSUpdates@" /etc/pamac.conf
+sudo sed -i "s@#NoUpdateHideIcon@RemoveUnrequiredDeps@" /etc/pamac.conf
+sudo sed -i "s@#DownloadUpdates@DownloadUpdates@" /etc/pamac.conf
+sudo sed -i "s@#RemoveUnrequiredDeps@RemoveUnrequiredDeps@" /etc/pamac.conf
+sudo sed -i "s@#EnableSnap@EnableSnap@" /etc/pamac.conf
+sudo sed -i "s@#EnableFlatpak@EnableFlatpak@" /etc/pamac.conf
+
 pamac install \
   asdf \
   fish \
