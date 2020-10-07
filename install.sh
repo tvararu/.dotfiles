4b70ffb97e9d29819e81456c1b3aaee6f06273f8
Theodor Vararu <theo@vararu.org>
2020-10-07 08:03:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sudo pacman -U https://archive.archlinux.org/packages/s/stow/stow-2.2.2-5-any.pkg.tar.xz
+# sudo pacman -U https://archive.archlinux.org/packages/s/stow/stow-2.2.2-5-any.pkg.tar.xz
 
-./cleanup.sh
+# stow --adopt */  # Replaces all existing dotfiles with symlinks to this repo
+# git reset --hard # Updates local dotfiles to be the same as in the repo
+# stow --restow */ # Installs remaining dotfiles
 
-stow --adopt */  # Replaces all existing dotfiles with symlinks to this repo
-git reset --hard # Updates local dotfiles to be the same as in the repo
-stow --restow */ # Installs remaining dotfiles
+# ./packages.sh
 
-./pamac.sh
+# chsh -s $(which fish)
