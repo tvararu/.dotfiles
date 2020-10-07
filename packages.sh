b4e3f473e4dff2703486d4282a399cf0d1c88e72
Theodor Vararu <theo@vararu.org>
2020-10-07 08:08:29 +0100

    Update scripts

---
+++ packages.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+yay -Sy network-manager-applet
+yay -Sy xss-lock
+yay -Sy dunst
+yay -Sy st-luke-git libxft-bgra-git
+yay -Sy asdf-vm fish hub htop flameshot telegram-desktop slack
+yay -Sy xorg-xset xorg-xrdb xcape
+yay -Sy ttf-fira-code tex-gyre-fonts ttf-apple-emoji otf-libertinus otf-san-francisco
+yay -Sy redshift python
+yay -Sy openssh
