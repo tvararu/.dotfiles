52517171ec3c47f9ee2a150ac1570d8a2a79d317
Theodor Vararu <theo@vararu.org>
2020-10-08 07:51:39 +0100

    Update packages list

--- packages.sh
+++ packages.sh
@@ -4,8 +4,10 @@ yay -Sy network-manager-applet
 yay -Sy xss-lock
 yay -Sy dunst
 yay -Sy st-luke-git libxft-bgra-git
-yay -Sy asdf-vm fish hub htop flameshot telegram-desktop slack
+yay -Sy asdf-vm fish hub htop flameshot telegram-desktop
 yay -Sy xorg-xset xorg-xrdb xcape
 yay -Sy ttf-fira-code tex-gyre-fonts ttf-apple-emoji otf-libertinus otf-san-francisco
 yay -Sy redshift python
 yay -Sy openssh
+yay -Sy atom
+yay -Sy logiops onboard bluez bluez-utils cheese alsa xorg-xrandr
