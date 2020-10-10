e93cd337b88a522c9ba8e680eb80072157e7bd64
Theodor Vararu <theo@vararu.org>
2020-10-10 23:00:59 +0100

    Reorg packages.sh

--- packages.sh
+++ packages.sh
@@ -1,17 +1,42 @@
 #!/bin/sh
 
-yay -Sy firefox vim
-yay -Sy xss-lock
-yay -Sy dunst
-yay -Sy st-luke-git libxft-bgra-git
-yay -Sy asdf-vm fish hub htop flameshot telegram-desktop
-yay -Sy xorg-xset xorg-xrdb xcape
-yay -Sy ttf-fira-code tex-gyre-fonts ttf-apple-emoji otf-libertinus otf-san-francisco
-yay -Sy redshift python
-yay -Sy openssh
-yay -Sy atom
-yay -Sy logiops onboard bluez bluez-utils cheese alsa xorg-xrandr
-yay -Sy lib32-mesa lib32-vulkan-radeon lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
-yay -Sy gnome-themes-extra
-yay -Sy steam
-yay -Sy xscreensaver
+yay -Sy alsa \
+  asdf-vm \
+  atom \
+  bluez \
+  bluez-utils \
+  cheese \
+  dunst \
+  firefox \
+  fish \
+  flameshot \
+  gnome-themes-extra \
+  htop \
+  hub \
+  lib32-libva-mesa-driver \
+  lib32-mesa \
+  lib32-mesa-vdpau \
+  lib32-vulkan-radeon \
+  libxft-bgra-git \
+  logiops \
+  mesa-vdpau \
+  onboard \
+  openssh \
+  otf-libertinus \
+  otf-san-francisco \
+  python \
+  redshift \
+  st-luke-git \
+  steam \
+  telegram-desktop \
+  tex-gyre-fonts \
+  ttf-apple-emoji \
+  ttf-fira-code \
+  vim \
+  xcape \
+  xorg-xrandr \
+  xorg-xrdb \
+  xorg-xset \
+  xscreensaver \
+  xscreensaver-aerial \
+  xss-lock
