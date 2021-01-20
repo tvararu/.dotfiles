3862338ff50af74ba80dcf75504ce32925847bdb
Theodor Vararu <theo@vararu.org>
2021-01-20 10:34:23 +0000

    Update asdf fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,14 @@
 set fish_greeting ""
 set -x EDITOR vim
 
-source /opt/asdf-vm/asdf.fish
+# Arch asdf config
+if test -e /opt/asdf-vm/asdf.fish
+  source /opt/asdf-vm/asdf.fish
+end
+
+# macOS asdf config
+if test -e /usr/local/opt/asdf/asdf.fish
+  source /usr/local/opt/asdf/asdf.fish
+end
+
 fish_ssh_agent
