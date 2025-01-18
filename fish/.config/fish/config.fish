dff1b400da2e82ef0e81681745edc116f92b7c04
Theodor Vararu <theo@vararu.org>
2025-01-18 23:45:27 +0000

    Remove fish asdf config
    I'm using mise now.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,16 +3,6 @@ set -x EDITOR vim
 
 fish_add_path ~/.bin
 
-# Arch asdf config
-if test -e /opt/asdf-vm/asdf.fish
-  source /opt/asdf-vm/asdf.fish
-end
-
-# macOS asdf config
-if test -e /opt/homebrew/opt/asdf/libexec/asdf.fish
-  source /opt/homebrew/opt/asdf/libexec/asdf.fish
-end
-
 fish_ssh_agent
 
 zoxide init fish | source
