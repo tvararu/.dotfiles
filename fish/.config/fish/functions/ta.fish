35969623e14d783303c9e0deb5d6b482a0c43636
Theodor Vararu <theo@vararu.org>
2025-10-31 10:51:29 +0700

    Fix ta
    This makes it properly attach to an existing session or create it if
    not.

--- fish/.config/fish/functions/ta.fish
+++ fish/.config/fish/functions/ta.fish
@@ -1,5 +1,7 @@
 function ta --description "Attach to tmux session 'main' or create it"
-    tmux has-session -t main 2>/dev/null
-    and tmux attach-session -t main
-    or tmux new-session -d -s main; and tmux attach-session -t main
+    if tmux has-session -t main 2>/dev/null
+        tmux attach-session -t main
+    else
+        tmux new-session -s main
+    end
 end
