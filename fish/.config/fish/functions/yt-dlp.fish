b1a935f2639bd26c6969caa95dc0f127705ecd73
Theodor Vararu <theo@vararu.org>
2026-08-30 19:05:47 +0100

    Make the yt-dlp config mount optional
    The image expects /download, and a missing config file used to make docker create a directory in its place.

--- fish/.config/fish/functions/yt-dlp.fish
+++ fish/.config/fish/functions/yt-dlp.fish
@@ -1,8 +1,6 @@
 function yt-dlp
-  docker run --rm \
-    --user (id -u):(id -g) \
-    -v "$PWD:/downloads" \
-    -v "$HOME/.config/yt-dlp/config:/etc/yt-dlp.conf:ro" \
-    ghcr.io/jim60105/yt-dlp \
-    $argv
+    set -l conf "$HOME/.config/yt-dlp/config"
+    set -l opts -v "$PWD:/download"
+    test -f $conf; and set -a opts -v "$conf:/etc/yt-dlp.conf:ro"
+    docker run --rm --user (id -u):(id -g) $opts ghcr.io/jim60105/yt-dlp $argv
 end
